4489dcfae59574173fed9ebfa1ce4767c818ffa2
Don <35393381+dyang32@users.noreply.github.com>
2018-02-27 01:52:54 -0600

    Add files via upload

---
+++ Week 7/week7-runlevel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Week 7 homework, Chapter 6
+# Don Yang
+# CSCI 2461-70 Computer Networking 2 - Linux
+
+# View system run level
+who -r
+
+# View system unit and configuration
+pkg-config system --varable=systemdsystemconfdir
+
+
